97eb0c740a023c6a82aa158fba376928625edebe
Kanat <kanatdzhumaliev@gmail.com>
2020-05-20 18:43:58 +0600

    init

---
+++ voip.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+
+const app = express();
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+// =======
+
+const server = app.listen(9000);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/myapp'
+});
+
+app.use('/static', express.static('public'))
+app.use('/peerjs', peerServer);
